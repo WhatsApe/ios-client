4bb109cfdc2a61aa55c8fc3d2e33c3657c822a75
Ricky Gill <rickygill@rickys-mbp>
2016-09-07 10:34:19 +0100

    Initial Commit

---
+++ ChitChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChitChat
+//
+//  Created by Ricky Gill on 07/09/2016.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
