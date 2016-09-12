ddeaa48fa3f292b4c76e9ed24023cfe4c2998ad5
alwinruby <alwinsolanky@gmail.com>
2016-09-12 12:39:36 +0100

    Defualt view is the recent chats

---
+++ ChitChat/TabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarController.swift
+//  ChitChat
+//
+//  Created by Alwin Solanky on 12/09/2016.
+//
+//
+
+import UIKit
+
+class TabBarController : UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        selectedIndex = 1
+        
+    }
+}
